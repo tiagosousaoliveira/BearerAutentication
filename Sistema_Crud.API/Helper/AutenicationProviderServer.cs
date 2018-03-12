d044f0709f7f82079893acb99defced29ef9c300
Tiago Sousa <tiagossousa61@gmail.com>
2018-03-12 11:27:31 -0300

    Implementação API baseada em OWIN com autenticação via Bearer

---
+++ Sistema_Crud.API/Helper/AutenicationProviderServer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Sistema_Crud.API.Helper
+{
+    public class AutenicationProviderServer : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            try
+            {
+                var username = context.UserName;
+                var password = context.Password;
+
+                if(username != "Tiago" && password != "123456" ){
+                    context.SetError("Dados Invalidos");
+                }
+
+                var roles = new List<string>();
+                roles.Add("User");
+
+
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, username));
+
+                GenericPrincipal principal = new GenericPrincipal(identity, roles.ToArray());
+                Thread.CurrentPrincipal = principal; ;
+
+                context.Validated(identity);
+
+
+            }
+            catch
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ Sistema_Crud.API/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Sistema_Crud.API.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Sistema_Crud.API
+{
+    public class Startup
+    {
+        public void configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            configureWebApi(config);
+            configureOAuth(app);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+        private void configureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuth = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/api/security/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(2),
+                Provider = new AutenicationProviderServer()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuth);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+        private void configureWebApi(HttpConfiguration config)
+        {
+            var JsonFormat = config.Formatters;
+            JsonFormat.Remove(JsonFormat.XmlFormatter);
+
+            var JsonSetings = JsonFormat.JsonFormatter.SerializerSettings;
+            JsonSetings.Formatting = Formatting.Indented;
+
+            JsonSetings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            JsonFormat.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+
+                name:"DefaultRoute",
+                routeTemplate:"api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional }
+                
+                );
+
+        }
+    }
+}
